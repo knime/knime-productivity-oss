d2d7f610abf061f774d72f2892304ab5ee3aa280
Thorsten Meinl <thorsten.meinl@knime.com>
2016-01-28 10:22:32 +0000

    fixed typo in error message

--- com.knime.explorer.nodes/src/com/knime/explorer/nodes/ExplorerBrowserNodeModel.java
+++ com.knime.explorer.nodes/src/com/knime/explorer/nodes/ExplorerBrowserNodeModel.java
@@ -2,7 +2,7 @@
   * This source code, its documentation and all appendant files
   * are protected by copyright law. All rights reserved.
   *
-  * Copyright by 
+  * Copyright by
   * KNIME.com, Zurich, Switzerland
   *
   * You may not modify, publish, transmit, transfer or sell, reproduce,
@@ -101,7 +101,7 @@ public class ExplorerBrowserNodeModel extends NodeModel {
         try {
             uri = new URI(url);
         } catch (URISyntaxException e) {
-            throw new InvalidSettingsException("Invalid URI provided \" +"
+            throw new InvalidSettingsException("Invalid URI provided \""
                     + url + "\".", e);
         }
         try {
